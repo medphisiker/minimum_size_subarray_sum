850160854655d0c48df84699868b45d6416e710f
Anton Shiryaev <med.phisiker@gmail.com>
2025-08-17 20:18:02 +1000

    первое решение с тестами

--- solution.py
+++ solution.py
@@ -1,6 +1,50 @@
 from typing import List
 
+# target = 4
+# window_sum = 4
+# window_len = 1
+# min_window_len = 1
+# 
+# 1 4 4
+#     l
+#       r
+
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        pass
+        left = 0
+        right = 0
+        window_sum = nums[0]
+        window_len = 1
+        min_window_len = len(nums) + 1
+
+        while right < len(nums):
+            if window_sum < target:
+                right += 1
+                
+                if right == len(nums):
+                    break
+
+                window_sum += nums[right]
+            else:
+                window_len = right - left + 1
+
+                if window_len < min_window_len:
+                    min_window_len = window_len
+
+                if left < right:
+                    window_sum -= nums[left]
+                    left += 1
+                else:
+                    right += 1
+                
+                    if right == len(nums):
+                        break
+
+                    window_sum += nums[right]
+        
+        # Если такого подмассива нет, верните 0.   
+        if min_window_len == len(nums) + 1:
+            min_window_len = 0
+
+        return min_window_len
---
+++ test.py
@@ -0,0 +1,66 @@
+from solution import Solution
+
+def test():
+    solution = Solution()
+    
+    # Пример 1 четное число элементов, подмассив из 2х элементов в конце
+    # target = 7
+    # window_sum = 7
+    # window_len = 2
+    # min_window_len = 2
+    #
+    # 2 3 1 2 4 3
+    #           l
+    #           r
+    
+    target = 7
+    nums = [2,3,1,2,4,3]
+    write_ans = 2
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    # Пример 2 нечетное число элементов, подмассив из 1го элемента в середине
+    target = 4
+    nums = [1,4,4]
+    write_ans = 1
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    # Пример 3 четное число элементов, подмассива с нужной суммой нет
+    target = 11
+    nums = [1,1,1,1,1,1,1,1]
+    write_ans = 0
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    # Тест массив из 1 элемента, нет подмассива
+    target = 5
+    nums = [1]
+    write_ans = 0
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    # Тест массив из 1 элемента, подмассив из 1 элемента
+    target = 1
+    nums = [1]
+    write_ans = 1
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    # Тест массив из 6 элементов, подмассив из 1 элемента
+    # дубли из чисел превышающих таргет
+    target = 5
+    nums = [1, 7, 7, 8, 1, 1]
+    write_ans = 1
+    
+    min_subarray_len = solution.minSubArrayLen(target, nums)
+    assert min_subarray_len == write_ans
+    
+    
+if __name__ == "__main__":
+    test()
